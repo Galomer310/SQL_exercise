6aaf8ab8721b76571a437503d92bceb5f96e06b6
Gal_omer <130598448+Galomer310@users.noreply.github.com>
2024-09-02 14:12:42 +0300

    Update SQL_Exercise.sql

--- SQL_Exercise.sql
+++ SQL_Exercise.sql
@@ -1,1 +1,25 @@
-SELECT * FROM customers WHERE first_name != 'Scott';
+CREATE TABLE items(item text, price smallint);
+
+INSERT INTO items(item, price) VALUES ('Small_Desk', 100),('Large_Desk', 300),('Fan', 80);
+
+CREATE TABLE customers(first_name text, last_name text);
+
+INSERT INTO customers(first_name, last_name) 
+VALUES 
+('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson');
+
+SELECT * FROM items;
+
+SELECT * FROM items WHERE price > 80;
+
+SELECT * FROM items WHERE price < 300;
+
+SELECT * FROM customers WHERE last_name != 'Smith;
+  
+SELECT * FROM customers WHERE last_name = 'Jones;
+
+SELECT * FROM customers WHERE first_name != 'Scott;
